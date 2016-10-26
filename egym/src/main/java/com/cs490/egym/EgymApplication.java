a6f575aa039e5d318cb3abbf0d84efa28827ef22
Michael <mdwranger@gmail.com>
2016-10-26 08:59:52 -0400

    init project

---
+++ egym/src/main/java/com/cs490/egym/EgymApplication.java
@@ -0,0 +1,12 @@
+package com.cs490.egym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EgymApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EgymApplication.class, args);
+	}
+}
