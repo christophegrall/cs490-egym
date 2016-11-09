28aca55ad08f1fe9dccb4e50378e20ed944fb27f
abstultz <abstultz@coastal.edu>
2016-11-09 18:58:26 -0500

    Commented out PreAuthorize Statements

--- egym/src/main/java/com/cs490/egym/controllers/FilterController.java
+++ egym/src/main/java/com/cs490/egym/controllers/FilterController.java
@@ -1,5 +1,6 @@
 package com.cs490.egym.controllers;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,10 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/api/filter", method = RequestMethod.GET)
 public class FilterController {
+	//@PreAuthorize("hasRole('USER')")
 	@RequestMapping("/filter1")
 	public void filter1() {
 		
 	}
+//	@PreAuthorize("hasRole('USER')")
 	@RequestMapping("/filter2")
 	public void filter2(){
 		
