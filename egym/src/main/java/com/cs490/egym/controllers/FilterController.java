128c735c988a0d91d48272d824201fbb60439730
abstultz <abstultz@coastal.edu>
2016-11-13 18:28:14 -0500

    Finished Filter

--- egym/src/main/java/com/cs490/egym/controllers/FilterController.java
+++ egym/src/main/java/com/cs490/egym/controllers/FilterController.java
@@ -8,6 +8,7 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cs490.egym.enums.DifficultyEnum;
@@ -20,32 +21,33 @@ public class FilterController {
 	@Autowired
 	private ExerciseRepository repo;
 	
-	String decision = null;
 	
 
 	
 
 	//@PreAuthorize("hasRole('USER')")
-	@RequestMapping("/filterType")
-	public List<ExerciseImpl> filterType() {
+	@RequestMapping("/type")
+	public List<ExerciseImpl> filterType(@RequestParam("type") String type) {
 		
-		String type = "WEIGHTLIFTING";
+//	String type = "CORE";
 		
-		List<ExerciseImpl> typeList = repo.getDifficulty(type);
+		List<ExerciseImpl> typeList = repo.getType(type);
 		
 //		System.out.println(diffList.toArray().toString());
 		return typeList;
 	}
 	
 //	@PreAuthorize("hasRole('USER')")
-	@RequestMapping("/filterDifficulty")
-	public void filterDifficulty(){
+	@RequestMapping("/difficulty")
+	public List<ExerciseImpl> filterDifficulty(@RequestParam("diff") String diff){
 		
-		String diff = "INTERMEDIATE";
+//		String diff = "INTERMEDIATE";
 		
 		List<ExerciseImpl> diffList = repo.getDifficulty(diff);
 		
-		System.out.println(diffList.toArray().toString());
+		return diffList;
+		
+//		System.out.println(diffList.toArray().toString());
 	}
 	
 }
--- egym/src/main/java/com/cs490/egym/repositories/ExerciseRepository.java
+++ egym/src/main/java/com/cs490/egym/repositories/ExerciseRepository.java
@@ -8,22 +8,15 @@ import org.springframework.data.jpa.repository.Query;
 import com.cs490.egym.models.ExerciseImpl;
 
 public interface ExerciseRepository extends JpaRepository<ExerciseImpl, Integer> {
-//	@Query(value="Select... ?1", nativeQuery=true)
-//	int[] advanced(String a);
-//	@Query(value="Select....", nativeQuery=true)
-//	int[] beginner(String b);
-//	@Query(value="Select....", nativeQuery=true)
-//	int[] intermediate(String i);
-//	@Query(value="Select...", nativeQuery=true)
-//	int[] expert(String e);
-	
+
 	
 	
 	@Query(value = "select * from exercise where difficulty = ?1", nativeQuery=true)
 	List<ExerciseImpl> getDifficulty(String diff);
 	
 	
-//	@Query(value = "select * from exercise where type = ?1", nativeQuery=true)
-	List<ExerciseImpl> findbyTYPE(String type);
+	@Query(value = "select * from exercise where type = ?1", nativeQuery=true)
+	List<ExerciseImpl> getType(String type);
+
 }
 
