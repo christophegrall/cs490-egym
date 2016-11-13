fbbd595713e813caf11a16053f9c4081fd6a83a6
abstultz <abstultz@coastal.edu>
2016-11-12 20:48:54 -0500

    Filter progress

--- egym/src/main/java/com/cs490/egym/repositories/ExerciseRepository.java
+++ egym/src/main/java/com/cs490/egym/repositories/ExerciseRepository.java
@@ -1,7 +1,24 @@
 package com.cs490.egym.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
 import com.cs490.egym.models.ExerciseImpl;
 
 public interface ExerciseRepository extends JpaRepository<ExerciseImpl, Integer> {
+//	@Query(value="Select... ?1", nativeQuery=true)
+//	int[] advanced(String a);
+//	@Query(value="Select....", nativeQuery=true)
+//	int[] beginner(String b);
+//	@Query(value="Select....", nativeQuery=true)
+//	int[] intermediate(String i);
+//	@Query(value="Select...", nativeQuery=true)
+//	int[] expert(String e);
+	@Query(value = "select * from exercise where difficulty = ?1", nativeQuery=true)
+	int[] getDifficulty(String diff);
+	
+	
+	@Query(value = "select* from exercise where type = ?1", nativeQuery=true)
+	int[] getType(String type);
 }
+
